c83e3edf72dc0e7cab496f493f474e2f4a58526d
Qodestackr <winchygichu@gmail.com>
2022-11-25 17:40:36 +0300

    add:more logos

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,40 @@
 import Title from './Common/Title';
 import { motion } from 'framer-motion';
 
+//
 import reactIMG from "../images/logos/react.svg"
-import nextjsIMG from "../images/logos/nextjs.svg"
+import VuejsIMG from "../images/logos/vue.svg"
+import reactNativeIMG from '../images/logos/react-native.svg'
 import typescriptIMG from "../images/logos/typescript.svg"
 import tailwindIMG from "../images/logos/tailwind.svg"
 import figmaIMG from "../images/logos/figma.svg"
-import firebaseIMG from '../images/logos/firebase.svg'
 import chakraUI_IMG from '../images/logos/chakraui.svg'
 
-let skills = [
+// nb
+import djangoIMG from '../images/logos/django.svg'
+import NestjsIMG from '../images/logos/nestjs.png'
+import graphqlIMG from '../images/logos/graphql.svg'
+import dockerIMG from '../images/logos/docker.svg'
+import gcpIMG from '../images/logos/gcp.png'
+import serverlessIMG from '../images/logos/serverless.png'
+import firebaseIMG from '../images/logos/firebase.svg'
+
+
+let frontend_skills = [
     {
         "src": reactIMG,
         "name": "React",
         "link": "https://reactjs.org/"
     },
     {
-        "src": nextjsIMG,
-        "name": "Next",
-        "link": "https://nextjs.org/"
+        "src": reactNativeIMG,
+        "name": "React Native",
+        "link": "https://firebase.google.com/"
+    },
+    {
+        "src": VuejsIMG,
+        "name": "Vue",
+        "link": "https://vuejs.org/"
     },
     {
         "src": typescriptIMG,
@@ -31,13 +47,46 @@ let skills = [
         "link": "https://tailwindcss.com/"
     },
     {
-    "src": chakraUI_IMG,
-            "name": "Tailwind",
-        "link": "https://chakraui.com/"
+        "src": chakraUI_IMG,
+        "name": "Tailwind",
+        "link": "https://chakra-ui.com/"
     },
     {
         "src": figmaIMG,
         "name": "Figma",
+        "link": "https://figma.com/"
+    }
+]
+
+let backend_skills = [
+    {
+        "src": djangoIMG,
+        "name": "Django",
+        "link": "https://reactjs.org/"
+    },
+    {
+        "src": NestjsIMG,
+        "name": "Nestjs",
+        "link": "https://vuejs.org/"
+    },
+    {
+        "src": graphqlIMG,
+        "name": "GraphQL",
+        "link": "https://www.typescriptlang.org/"
+    },
+    {
+        "src": dockerIMG,
+        "name": "Docker",
+        "link": "https://tailwindcss.com/"
+    },
+    {
+        "src": gcpIMG,
+        "name": "GCP",
+        "link": "https://chakra-ui.com/"
+    },
+    {
+        "src": serverlessIMG,
+        "name": "Serverless",
         "link": "https://figma.com/"
     },
     {
@@ -46,7 +95,6 @@ let skills = [
         "link": "https://firebase.google.com/"
     }
 ]
-
 
 // [React, Vue, React Native, Chakra UI, Tailwind CSS, Figma]
 // [AWS, GCP, Firebase, Django, Node.js, Serverless]
@@ -57,7 +105,7 @@ const Skills = () => {
             <Title num={4} title="Frontend & Mobile" />
 
             <div className="flex flex-wrap items-center justify-center mt-10 space-x-6 md:space-x-10 md:items-start md:justify-start">
-                {skills?.map((skill, i) => (
+                {frontend_skills?.map((skill, i) => (
                     <motion.a
                         className="cursor-pointer relative w-20 h-20"
                         href={skill.link}
@@ -86,7 +134,7 @@ const Skills = () => {
             {/*  */}
             <Title num={'5'} title="Backend Stack" />
             <div className="flex flex-wrap items-center justify-center mt-10 space-x-6 md:space-x-10 md:items-start md:justify-start">
-                {skills?.map((skill, i) => (
+                {backend_skills?.map((skill, i) => (
                     <motion.a
                         className="cursor-pointer relative w-20 h-20"
                         href={skill.link}
