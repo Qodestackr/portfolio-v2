b0b67bbe0312cf4bb0481887c9b23eaeb76ee1fc
Qodestackr <winchygichu@gmail.com>
2023-11-10 00:08:07 +0300

    __nbn

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Projects from './components/Projects'
 import Testimonials from "./components/Testimonials";
 import Contact from './components/Contact';
 import Blog from './components/Blog/Blog'
+import PDFViewer from './PDFViewer';
 
-import resumeURL from './resume.pdf'
 
 function App() {
   return (
@@ -22,11 +22,7 @@ function App() {
       {/* <Blog /> */}
       <Contact />
 
-      <object data={resumeURL} type="application/pdf" width="100%" height="100%">
-        <p>Alternative text - include a link <a href="https://drive.google.com/file/d/188mesbieUkxT4rvn-g7dwz3jLQvY_TBT/view">to the PDF!</a></p>
-        Hello, 
-      </object>
-
+      <PDFViewer />
     </Layout>
   )
 }
---
+++ src/PDFViewer.jsx
@@ -0,0 +1,10 @@
+
+const PDFViewer = () => {
+ return (
+ <div>
+ <iframe src={'https://github.com/Qodestackr/portfolio-v2/blob/master/resume.pdf'} width='100%' height='500px' />
+ </div>
+ );
+};
+
+export default PDFViewer;
